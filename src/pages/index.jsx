ad06e2c12b5d03ab85036e7c3d1b5661bc2323d1
Harshna Goratela <harshna.goratela@gmail.com>
2020-07-03 20:56:15 +0530

    resolve build error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,21 @@
 import React from "react"
 import { navigate } from "gatsby"
-import { isLoggedIn } from "../utils/auth"
+import { isLoggedIn, isBrowser } from "../utils/auth"
 
 const Index = ({location}) => {
-  if (!isLoggedIn() && location.pathname !== `/dashboard/login`) {
+  if (isBrowser() && !isLoggedIn() && location.pathname !== `/dashboard/login`) {
     // If we’re not logged in, redirect to the login page.
     navigate(`/dashboard/login`, { replace: true })
     return null
   }
-  if(isLoggedIn()) {
+  if(isBrowser() && isLoggedIn()) {
     // If we’re logged in, redirect to the dashboard page.
     navigate(`/dashboard/`, { replace: true })
     return null;
   }
+  return (
+    <>
+    </>
+  );
 }
 export default Index
