fdd4082a381e35a4bef5ae5c492dab123fff8595
Harshna Goratela <harshna.goratela@gmail.com>
2020-07-04 14:12:40 +0530

    dashboard icons color and shape corrected

--- src/components/PageList.jsx
+++ src/components/PageList.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 import { Avatar } from "evergreen-ui"
+import _ from 'lodash'
 
 const PageList = ({projects}) => {
+
+    let bgColors = ["id_170","id_174","id_177","id_173","id_175","id_171"]
+    //bgColors = [];
+    let start = "170"
+    _.times(20, () => {
+        //bgColors.push("id_"+(start++));
+      });
 
     return (
         <div className="row">
@@ -9,7 +17,7 @@ const PageList = ({projects}) => {
                 <div className="col col5">
                     <div className="dashboard_box text-center">
                         <a href="javascript:void(0)">
-                            <Avatar name={project.title} isSolid size={100} />
+                            <Avatar name={project.title} hashValue={bgColors[index%6]} isSolid size={100} style={{borderRadius : "12px"}}/>
                             <h3>{project.title}</h3>
                         </a>
                     </div>
